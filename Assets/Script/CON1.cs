e122cec33de400f83cca4e5ed1d10a1fba18961e
sdh6651 <44226565+sdh6651@users.noreply.github.com>
2018-10-18 10:12:55 +0900

    Initial commit

---
+++ Assets/Script/CON1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CON1 : MonoBehaviour
+{
+    Camera camera;
+    public GameObject tagetObj;
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                tagetObj = hit.transform.gameObject;
+                tagetObj.GetComponent<Pawn>().pOnOff = true;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Pawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour {
+    public bool pOnOff;
+    public GameObject movep;
+    public GameObject sel;
+    GameObject whoObj;
+    public float my;
+    public float mx;
+    public float mz;
+    public GameObject cameracon;
+   
+	void Update ()
+    {
+        if (pOnOff == true)
+        {
+            Instantiate(sel, transform.position,transform.rotation);
+            whoObj = cameracon.GetComponent<CON1>().tagetObj;
+            my = whoObj.transform.position.y+0.15f;
+            mx = whoObj.transform.position.x;
+            mz = whoObj.transform.position.z+0.5f;
+            Instantiate(movep,new Vector3(mx,my,mz),Quaternion.Euler(0,0,0));
+            mz = whoObj.transform.position.z + 1;
+            Instantiate(movep,new Vector3(mx, my, mz),Quaternion.Euler(0, 0, 0));
+            pOnOff = false;
+        }
+	}
+}
